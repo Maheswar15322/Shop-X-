d897e2ba7800f13885e336013447e46e6880f3bf
Maheswar <mahimandal8617@gmail.com>
2026-07-13 19:47:18 +0530

    Update Theme.css

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { CartProvider } from "./Context/CartContext";
 import { WishlistProvider } from "./Context/WishlistContext";
 import { Toaster } from "react-hot-toast";
 import { ThemeProvider } from "./Context/ThemeContext";
-import "./theme.css";
+import "./Theme.css";
 import { AuthProvider } from "./Context/AuthContext";
 
 
